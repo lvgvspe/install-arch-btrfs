1d8d5d65bf8487f102bb32508b3ce19df3f93fe4
Lucas Camargo <lvgvspe@icloud.com>
2025-02-22 05:16:09 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,19 +1,17 @@
-
-
 # Configurar o fuso horário
 echo "Configurando o fuso horário para America/Sao_Paulo..."
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
 
 # Configurar o locale
-echo "Configurando o locale para pt_BR..."
+echo "Configurando o locale para pt_BR.UTF-8..."
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
 
-# Configurar o teclado
-echo "Configurando o teclado para br-abnt2..."
-echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
+# Configurar o layout do teclado
+echo "Configurando o layout do teclado para BR-ABNT..."
+echo "KEYMAP=br-abnt" > /etc/vconsole.conf
 
 # Configurar o hostname
 echo "Por favor, insira o nome do host:"
@@ -30,14 +28,33 @@ echo "127.0.1.1   $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 echo "Definindo a senha do root..."
 passwd
 
-# Instalar o bootloader (GRUB)
+# Criar um novo usuário
+echo "Criando um novo usuário..."
+echo "Por favor, insira o nome de usuário:"
+read USERNAME
+useradd -m -G wheel -s /bin/bash $USERNAME
+echo "Por favor, defina uma senha para o novo usuário:"
+passwd $USERNAME
+
+# Instalar pacotes adicionais (opcional)
+echo "Instalando pacotes adicionais (opcional)..."
+pacman -S networkmanager sudo vim --noconfirm
+
+# Habilitar o NetworkManager
+echo "Habilitando o NetworkManager..."
+systemctl enable NetworkManager
+
+# Configurar o sudo para o novo usuário
+echo "Configurando o sudo para o novo usuário..."
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Instalar o GRUB (bootloader)
 echo "Instalando o GRUB..."
-pacman -S grub
-echo "Por favor, insira o disco que deseja instalar o GRUB (ex: /dev/sda):"
-read DISK
-grub-install --target=i386-pc $DISK
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB  
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Finalizar a instalação
 echo "Instalação concluída!"
-echo "Digite 'exit' para sair do chroot e reinicie o sistema."
+echo "Agora você pode reiniciar o sistema:"
+echo "1. Digite 'exit' para sair do ambiente chroot."
+echo "2. Execute 'reboot' para reiniciar o sistema."
